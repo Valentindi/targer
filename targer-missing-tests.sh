147c170ee1b651cb326417fa4c7e69af48f9b053
Valentin Dittmar <valentin.dittmar@student.uni-halle.de>
2020-01-18 16:07:41 +0100

    add dockerfile and new run-script

---
+++ targer-missing-tests.sh
@@ -0,0 +1,120 @@
+export PYTHONIOENCODING=utf8
+
+train="../wstud-thesis-dittmar/data/targer/targer_train.txt"
+dev="../wstud-thesis-dittmar/data/targer/targer_dev.txt"
+test="../wstud-thesis-dittmar/data/targer/targer_test.txt"
+targetdir="../wstud-thesis-dittmar/targer-results"
+
+e=1000
+
+eval='f1-alpha-match-05'
+
+# TODOs:  
+#	--model {BiRNN,BiRNNCNN,BiRNNCRF,BiRNNCNNCRF}
+# 	--opt {sgd,adam} x
+# 	--LR {0.01, 0.001, 0.1, 1}
+# 	--rnn-type {Vanilla,LSTM,GRU} x
+# 	--rnn-hidden-dim RNN_HIDDEN_DIM {10, 100, 1000}
+
+cp -f ../glove.6B.100d.txt embeddings/glove.6B.100d.txt
+
+name="run-hidden-200"
+python3 main.py --train $train --dev $dev --test $test --save $name.hdf5 --rnn-hidden-dim 200  --save-best yes --cross-folds-num 5 -v $eval -p 25 -e $e
+python3 run_tagger.py $name.hdf5 $test -g -1 -o $name.json
+python3 eval.py $test $name.json $name.eval.txt
+python3 generate_detailed_report.py $test $name.json $name.report.html
+
+mv $name* targetdir
+
+
+
+cp -f ../glove.6B.100d.txt embeddings/glove.6B.100d.txt
+
+name="run-hidden-300"
+python3 main.py --train $train --dev $dev --test $test --save $name.hdf5 --rnn-hidden-dim 300  --save-best yes --cross-folds-num 5 -v $eval -p 25 -e $e
+python3 run_tagger.py $name.hdf5 $test -g -1 -o $name.json
+python3 eval.py $test $name.json $name.eval.txt
+python3 generate_detailed_report.py $test $name.json $name.report.html
+
+mv $name* targetdir
+
+
+
+
+cp -f ../glove.6B.100d.txt embeddings/glove.6B.100d.txt
+
+name="run-hidden-400"
+python3 main.py --train $train --dev $dev --test $test --save $name.hdf5 --rnn-hidden-dim 400  --save-best yes --cross-folds-num 5 -v $eval -p 25 -e $e
+python3 run_tagger.py $name.hdf5 $test -g -1 -o $name.json
+python3 eval.py $test $name.json $name.eval.txt
+python3 generate_detailed_report.py $test $name.json $name.report.html
+
+mv $name* targetdir
+
+
+
+
+cp -f ../glove.6B.100d.txt embeddings/glove.6B.100d.txt
+
+name="run-hidden-666"
+python3 main.py --train $train --dev $dev --test $test --save $name.hdf5 --rnn-hidden-dim 666  --save-best yes --cross-folds-num 5 -v $eval -p 25 -e $e
+python3 run_tagger.py $name.hdf5 $test -g -1 -o $name.json
+python3 eval.py $test $name.json $name.eval.txt
+python3 generate_detailed_report.py $test $name.json $name.report.html
+
+mv $name* targetdir
+
+
+
+cp -f ../glove.6B.100d.txt embeddings/glove.6B.100d.txt
+
+name="lr-0.001"
+python3 main.py --train $train --dev $dev --test $test --save $name.hdf5 --lr 0.001  --save-best yes --cross-folds-num 5 -v $eval -p 25 -e $e
+python3 run_tagger.py $name.hdf5 $test -g -1 -o $name.json
+python3 eval.py $test $name.json $name.eval.txt
+python3 generate_detailed_report.py $test $name.json $name.report.html
+
+mv $name* targetdir
+
+
+
+
+cp -f ../glove.6B.100d.txt embeddings/glove.6B.100d.txt
+
+name="lr-0.005"
+python3 main.py --train $train --dev $dev --test $test --save $name.hdf5 --lr 0.005  --save-best yes --cross-folds-num 5 -v $eval -p 25 -e $e
+python3 run_tagger.py $name.hdf5 $test -g -1 -o $name.json
+python3 eval.py $test $name.json $name.eval.txt
+python3 generate_detailed_report.py $test $name.json $name.report.html
+
+mv $name* targetdir
+
+
+
+cp -f ../glove.6B.100d.txt embeddings/glove.6B.100d.txt
+
+name="lr-0.05"
+python3 main.py --train $train --dev $dev --test $test --save $name.hdf5 --lr 0.05  --save-best yes --cross-folds-num 5 -v $eval -p 25 -e $e
+python3 run_tagger.py $name.hdf5 $test -g -1 -o $name.json
+python3 eval.py $test $name.json $name.eval.txt
+python3 generate_detailed_report.py $test $name.json $name.report.html
+
+mv $name* targetdir
+
+
+
+name="run-BiRNNCRF-hidden-500-GRO"
+python3 main.py --train $train --dev $dev --test $test --save $name.hdf5  --model BiRNNCRF --rnn-type=GRU --rnn-hidden-dim 500   --save-best yes --cross-folds-num 5 -v $eval -p 25 -e $e
+python3 run_tagger.py $name.hdf5 $test -g -1 -o $name.json
+python3 eval.py $test $name.json $name.eval.txt
+python3 generate_detailed_report.py $test $name.json $name.report.html
+
+mv $name* targetdir
+
+
+
+
+
+
+
+
