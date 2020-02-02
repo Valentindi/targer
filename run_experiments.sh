65a89ce559cebe58ad546e76dc54b9de086493b2
Valentin Dittmar <valentin.dittmar@student.uni-halle.de>
2020-02-02 16:50:47 +0100

    add script for runnging experiments for bert-base and bert-large

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,20 @@
-python -m spacy download en_core_web_sm
-python -m spacy download en
-export PYTHONIOENCODING=utf8
-cd pretrained
-mkdir models
-python load_model.py --model-dir ./models/ --logname load_model.log
-cd ..
-python main.py --train data/targer/targer_train.txt --dev data/targer/targer_dev.txt --test data/targer/targer_test.txt  --data-io connl-ner-2003 --model BiRNN --opt adam --save-best yes --patience 20 --rnn-hidden-dim 200 --gpu 2 --elmo False --epoch-num 50 --evaluator f1-macro --bert True --path_to_bert ./pretrained/models/uncased_L-12_H-768_A-12/ --logname bert-768
 
+
+GPU_1="2"
+GPU_2="3"
+
+MODEL_BERT_BASE="--path_to_bert ./pretrained/models/uncased_L-12_H-768_A-12 --embedding-dim 1024"
+MODEL_BERT_LARGE="--path_to_bert ./pretrained/models/uncased_bert-large --embedding-dim 1024"
+
+DATA_TRAIN="data/targer/targer_train.txt"
+DATA_TEST="data/targer/targer_test.txt"
+DATA_DEV="data/targer/targer_dev.txt"
+DATA="--train $DATA_TRAIN --dev $DATA_DEV --test $DATA_TEST --data-io connl-ner-2003"
+
+PATENCE="20"
+RNN_HIDDEN_DIM="200"
+
+# comparision of models
+
+python main.py  $DATA  --model BiRNN --opt adam --save-best yes --patience $PATENCE --rnn-hidden-dim $RNN_HIDDEN_DIM --gpu $GPU_1 --elmo False --epoch-num 50 --evaluator f1-macro --bert True $MODEL_BERT_BASE --logname bert-base.log --report-fn bert-base.txt &
+python main.py $DATA --model BiRNN --opt adam --save-best yes --patience $PATENCE --rnn-hidden-dim $RNN_HIDDEN_DIM --gpu $GPU_2 --elmo False --epoch-num 50 --evaluator f1-macro --bert True  $MODEL_BERT_LARGE --logname bert-large.log --report-fn bert-large.txt &
