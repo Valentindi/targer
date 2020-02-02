de1a441fd3e058ecf173c03e475457c2ca33264d
Your Name <docker@valentindittmar.eu>
2020-02-02 19:00:53 +0000

    add run scripts

--- download_models.sh
+++ download_models.sh
@@ -5,7 +5,7 @@ cd pretrained
 mkdir models
 python load_model.py --model-dir ./models/ --logname load_model.log
 
-mkdir bert-large-uncased
+mkdir bert-large-uncased && cd bert-large-uncased
 wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-uncased-pytorch_model.bin -O pytorch_model.bin
 wget wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-uncased-vocab.txt -O vocab.txt
  wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-large-uncased-config.json -O bert_config.json
---
+++ run_experiments-targer.sh
@@ -0,0 +1,19 @@
+
+
+GPU_1="2"
+GPU_2="3"
+
+MODEL_BERT_BASE="--path_to_bert ./pretrained/models/uncased_L-12_H-768_A-12 --embedding-dim 768"
+MODEL_BERT_LARGE="--path_to_bert ./pretrained/models/uncased_bert-large --embedding-dim 1024"
+
+DATA_TRAIN="data/targer/targer_train.txt"
+DATA_TEST="data/targer/targer_test.txt"
+DATA_DEV="data/targer/targer_dev.txt"
+DATA="--train $DATA_TRAIN --dev $DATA_DEV --test $DATA_TEST --data-io connl-ner-2003"
+
+PATENCE="20"
+RNN_HIDDEN_DIM="200"
+
+# comparision of models
+
+python main.py  $DATA  --model BiRNN --opt adam --save-best yes --patience $PATENCE --rnn-hidden-dim $RNN_HIDDEN_DIM --gpu $GPU_1 --elmo False --epoch-num 50 --evaluator f1-macro --bert False --logname targer.log --report-fn targer.txt
