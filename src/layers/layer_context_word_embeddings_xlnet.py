39b981e499ec3d1c57c202c7dd784136b8a3ca2c
Valentin Dittmar <valentin.dittmar@student.uni-halle.de>
2020-02-05 11:21:04 +0100

    fix xlnet

--- src/layers/layer_context_word_embeddings_xlnet.py
+++ src/layers/layer_context_word_embeddings_xlnet.py
@@ -39,54 +39,43 @@ class LayerContextWordEmbeddingsXlNet(LayerBase):
         
 
     def forward(self, word_sequences):
-        character_ids = self.to_gpu(self.word_seq_indexer.batch_to_ids(word_sequences))
-        word_embeddings_feature = self.embeddings(character_ids) # shape: batch_size x max_seq_len x output_dim
-        self.weight = word_embeddings_feature
-        return word_embeddings_feature
 
-        #
-        # tokens_tensor, segments_tensor, number_word_in_seq = self.word_seq_indexer.batch_to_ids(word_sequences)
-        #
-        # tokens_tensor = self.to_gpu(tokens_tensor)
-        # segments_tensor = self.to_gpu(segments_tensor)
-        # self.word_sequence  = word_sequences
-        # self.token_tensor = tokens_tensor
-        # self.segment_tensor = segments_tensor
-        #
-        # encoded_layers = self.embeddings(self.token_tensor, self.segment_tensor)
-        #
-        # batch_embeddings = []
-        # for batch_i in range(self.token_tensor.shape[0]): #batch_size
-        #     token_embeddings = []
-        #     for token_i in range(self.token_tensor.shape[1]):  #number of token in batch element
-        #         hidden_layers = []
-        #         for layer_i in range(len(encoded_layers)):
-        #             vec = encoded_layers[layer_i][batch_i][token_i]
-        #             hidden_layers.append(vec)
-        #         token_embeddings.append(hidden_layers)
-        #     summed_last_4_layers = [torch.sum(torch.stack(layer)[-4:], 0) for layer in token_embeddings]
-        #     summed_last_4_layers = torch.stack(summed_last_4_layers)
-        #     batch_embeddings.append(summed_last_4_layers)
-        #
-        # answer = torch.stack(batch_embeddings)
-        #
-        # max_seq_len = max([len(word_seq) for word_seq in word_sequences])
-        # index =  self.to_gpu(number_word_in_seq)
-        # index = index.unsqueeze(2)
-        # index = index.repeat(1, 1, answer.shape[2])
-        #
-        # self_tensor = torch.zeros(index.shape) # batch_size*max_num_word (word! not token!)*len_embedding
-        # # why + 4?
-        #
-        # self_tensor = self.to_gpu(self_tensor)
-        #
-        # #torch.save([index], 'index.pth')
-        # #torch.save([answer], 'answer.pth')
-        # #torch.save([self_tensor], 'self_tensor.pth')
-        # #logging.info("scatter add {}\n====\n{}\n====\n{}".format(self_tensor, index, answer))
-        # #logging.info("scatter add {}\n====\n{}\n====\n{}".format(self_tensor, index.shape, answer.shape))
-        # self_tensor1 = self_tensor.scatter_add_(1, index, answer)
-        # #torch.save([self_tensor1], 'self_tensor1.pth')
-        # self_tensor1 = self_tensor1[:, 1:max_seq_len]
-        # self_tensor1 = F.pad(self_tensor1, (0, 0, 0, max_seq_len - self_tensor1.shape[1], 0, 0), "constant", 0)
-        # return self_tensor1
+
+        tokens_tensor, segments_tensor, number_word_in_seq = self.word_seq_indexer.batch_to_ids(word_sequences)
+        self.to_gpu(tokens_tensor)
+        self.to_gpu(segments_tensor)
+        self.to_gpu(number_word_in_seq)
+        encoded_layers = self.embeddings(tokens_tensor, segments_tensor)
+
+
+
+
+        batch_embeddings = []
+        for batch_i in range(tokens_tensor.shape[0]): #batch_size
+            token_embeddings = []
+            for token_i in range(tokens_tensor.shape[1]):  #number of token in batch element
+                hidden_layers = []
+                for layer_i in range(len(encoded_layers)):
+                    vec = encoded_layers[layer_i][batch_i][token_i]
+                    hidden_layers.append(vec)
+                token_embeddings.append(hidden_layers)
+            summed_last_4_layers = [torch.sum(torch.stack(layer)[-4:], 0) for layer in token_embeddings]
+            summed_last_4_layers = torch.stack(summed_last_4_layers)
+            batch_embeddings.append(summed_last_4_layers)
+
+        answer = torch.stack(batch_embeddings)
+
+        max_seq_len = max([len(word_seq) for word_seq in word_sequences])
+        index =  self.to_gpu(number_word_in_seq)
+        index = index.unsqueeze(2)
+        index = index.repeat(1, 1, answer.shape[2])
+
+        self_tensor = torch.zeros(index.shape) # batch_size*max_num_word (word! not token!)*len_embedding
+
+        self_tensor = self.to_gpu(self_tensor)
+
+        self_tensor1 = self_tensor.scatter_add_(1, index, answer)
+
+        self_tensor1 = self_tensor1[:, 1:max_seq_len]
+        self_tensor1 = F.pad(self_tensor1, (0, 0, 0, max_seq_len - self_tensor1.shape[1], 0, 0), "constant", 0)
+        return self_tensor1
